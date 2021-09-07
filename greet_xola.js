1fd400e9ef97ba0329ded80e1c99a504fb1f01ce
Mawande <mawandesikibi.com>
2021-09-07 17:54:24 +0200

    initial commit

--- greet_xola.js
+++ greet_xola.js
@@ -1,4 +1,6 @@
+const chalk = require('chalk');
 //import the greet module that is in the current folder
 const greet = require('./greet');
 
-console.log(greet('Xola'))
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
